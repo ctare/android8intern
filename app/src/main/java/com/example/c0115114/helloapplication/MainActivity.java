f0ebac54d46879176fe1d490530bd2a3bd680e93
ctare <sankaku900622@yahoo.co.jp>
2017-03-06 12:21:59 +0900

    init blank activity

---
+++ app/src/main/java/com/example/c0115114/helloapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.c0115114.helloapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
