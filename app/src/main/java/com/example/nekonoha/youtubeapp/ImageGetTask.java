5a82ba2c1e41701b11454e6bca4966cd8f1338fb
nekonoha <nekonoha>
2017-03-07 19:36:31 +0900

    progress

--- app/src/main/java/com/example/nekonoha/youtubeapp/ImageGetTask.java
+++ app/src/main/java/com/example/nekonoha/youtubeapp/ImageGetTask.java
@@ -2,30 +2,31 @@ package com.example.nekonoha.youtubeapp;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.os.AsyncTask;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- * Created by c0115114 on 2017/03/07.
- */
 
 public class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
     private ImageView image;
+    private ProgressBar progressBar;
 
-    public ImageGetTask(ImageView img){
+    public ImageGetTask(ImageView img, ProgressBar p){
         image = img;
+        progressBar = p;
     }
 
     @Override
     protected Bitmap doInBackground(String... params) {
         Bitmap image;
         try {
+            Thread.sleep(10);
             URL imageUrl = new URL(params[0]);
             InputStream imageIs;
             imageIs = imageUrl.openStream();
@@ -35,10 +36,14 @@ public class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
             return null;
         } catch (IOException e) {
             return null;
+        } catch (InterruptedException e) {
+            return null;
         }
     }
+
     @Override
     protected void onPostExecute(Bitmap result) {
+        progressBar.setVisibility(View.INVISIBLE);
         // 取得した画像をImageViewに設定します。
         image.setImageBitmap(result);
     }
