5d7f3beae49523a63f3250399f26fa42edae83cd
nekonoha <nekonoha>
2017-03-08 17:46:21 +0900

    youtube Fragment, activity

--- app/src/main/java/com/example/nekonoha/youtubeapp/DisplayActivity.java
+++ app/src/main/java/com/example/nekonoha/youtubeapp/DisplayActivity.java
@@ -1,7 +1,10 @@
 package com.example.nekonoha.youtubeapp;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
@@ -16,6 +19,36 @@ public class DisplayActivity extends AppCompatActivity {
 
         title.setText("Title");
         caption.setText("Caption");
+
+        // フラグメント起動 （v4の作法で）
+        YoutubeFragment fragment = new YoutubeFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.main, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
---
+++ app/src/main/java/com/example/nekonoha/youtubeapp/YoutubeFragment.java
@@ -0,0 +1,62 @@
+package com.example.nekonoha.youtubeapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+
+public class YoutubeFragment extends Fragment {
+    private static final String API_KEY = "AIzaSyCg-nDKbwKIzh0W43xRHSV0HnLD-SVhR4w";
+    private static String VIDEO_ID = "N4mEzFDjqtA";
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView  = inflater.inflate(R.layout.fragment_youtube, container, false);
+
+        // YouTubeフラグメントインスタンスを取得
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+
+        // レイアウトにYouTubeフラグメントを追加
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_layout, youTubePlayerFragment).commit();
+
+        // YouTubeフラグメントのプレーヤーを初期化する
+        youTubePlayerFragment.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+
+            // YouTubeプレーヤーの初期化成功
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+                if (!wasRestored) {
+                    player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+                    player.loadVideo(VIDEO_ID);
+
+                    player.play();
+                }
+            }
+
+            // YouTubeプレーヤーの初期化失敗
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult error) {
+                // YouTube error
+                String errorMessage = error.toString();
+                Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
+                Log.d("errorMessage:", errorMessage);
+            }
+        });
+
+        return rootView;
+    }
+}
