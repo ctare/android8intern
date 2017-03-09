8992a5c29185d20c602a91c627a308b8a43bc696
ctare <sankaku900622@yahoo.co.jp>
2017-03-09 12:56:10 +0900

    on create view

--- app/src/main/java/com/example/nekonoha/youtubeapp/TopFragment.java
+++ app/src/main/java/com/example/nekonoha/youtubeapp/TopFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by c0115114 on 2017/03/09.
@@ -18,15 +19,19 @@ public class TopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_top, container);
-        ViewGroup.LayoutParams inner = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        ViewGroup.LayoutParams outer = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        ViewGroup.LayoutParams inner = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         for (int j = 0; j < 3; j++) {
             LinearLayout thumbnails = new LinearLayout(getActivity());
+            thumbnails.setOrientation(LinearLayout.HORIZONTAL);
             for (int i = 0; i < 2; i++) {
-                View thumbnail = inflater.inflate(R.layout.fragment_thumbnail, container);
-                thumbnails.addView(thumbnail);
+                View thumbnail = new ThumbnailFragment().onCreateView(inflater, container, savedInstanceState);
+                TextView tv = new TextView(getActivity());
+                tv.setText("test");
+                thumbnails.addView(thumbnail, inner);
             }
             LinearLayout thumbnails_wrap = (LinearLayout) view.findViewById(R.id.thumbnails);
-            thumbnails_wrap.addView(thumbnails);
+            thumbnails_wrap.addView(thumbnails, outer);
         }
         return view;
     }
