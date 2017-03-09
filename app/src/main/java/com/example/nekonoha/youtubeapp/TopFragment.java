feba7213a8982ac35263670e3a38c23f5bf0b314
ctare <sankaku900622@yahoo.co.jp>
2017-03-09 14:45:27 +0900

    layout

--- app/src/main/java/com/example/nekonoha/youtubeapp/TopFragment.java
+++ app/src/main/java/com/example/nekonoha/youtubeapp/TopFragment.java
@@ -4,6 +4,7 @@ package com.example.nekonoha.youtubeapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,18 +20,18 @@ public class TopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_top, container);
-        ViewGroup.LayoutParams outer = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        ViewGroup.LayoutParams inner = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams outer = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams inner = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+        LinearLayout thumbnails_wrap = (LinearLayout) view.findViewById(R.id.thumbnails);
         for (int j = 0; j < 3; j++) {
             LinearLayout thumbnails = new LinearLayout(getActivity());
             thumbnails.setOrientation(LinearLayout.HORIZONTAL);
             for (int i = 0; i < 2; i++) {
                 View thumbnail = new ThumbnailFragment().onCreateView(inflater, container, savedInstanceState);
-                TextView tv = new TextView(getActivity());
-                tv.setText("test");
+//                thumbnail.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+//                thumbnail.setWidth(0);
                 thumbnails.addView(thumbnail, inner);
             }
-            LinearLayout thumbnails_wrap = (LinearLayout) view.findViewById(R.id.thumbnails);
             thumbnails_wrap.addView(thumbnails, outer);
         }
         return view;
