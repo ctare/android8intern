e55f7cb273749bb70b81a31fe6b10a5dd1749e58
Nekonoha <tan.fantazma@gmail.com>
2017-03-13 11:59:44 +0900

    layout

--- app/src/main/java/com/example/nekonoha/youtubeapp/SettingsFragment.java
+++ app/src/main/java/com/example/nekonoha/youtubeapp/SettingsFragment.java
@@ -13,8 +13,10 @@ import android.view.animation.Interpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -24,7 +26,16 @@ public class SettingsFragment extends Fragment {
     TextView results;
     Button s_settings_button;
     TableLayout search_settings;
+
+    SeekBar gyro_sensitivity_seekBar;
+    TextView gyro_sensitivity;
+    Button sensor_settings_button;
+    TableLayout sensor_settings;
+
+    Switch gyro_switch;
+
     boolean isOpendSearch = true;
+    boolean isOpendSensor = true;
     private int ANIMATION_DURATION = 500;
     static final Interpolator FAST_OUT_SLOW_IN_INTERPOLATOR = new FastOutSlowInInterpolator();
     private Spinner sort;
@@ -48,6 +59,8 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        //検索結果の件数
         seekBar = (SeekBar) getActivity().findViewById(R.id.resultSeekBar);
         results = (TextView) getActivity().findViewById(R.id.results);
 
@@ -55,7 +68,7 @@ public class SettingsFragment extends Fragment {
         final SettingsData settingsData = SettingsDataStatic.getInstance();
         Integer now = settingsData.getSearchLimit();
         results.setText(now.toString() + "件");
-        seekBar.setProgress(now / 2  - 1);
+        seekBar.setProgress(now / 2 - 1);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -80,53 +93,13 @@ public class SettingsFragment extends Fragment {
             }
         });
 
-        //検索設定
+        //ソート
 
-        s_settings_button = (Button) getActivity().findViewById(R.id.s_settings_button);
-        s_settings_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                search_settings = (TableLayout) getActivity().findViewById(R.id.search_settings);
-
-                if (isOpendSearch) {
-                    ViewCompat.animate(search_settings)
-                            .alpha(0f)
-//                            .translationY(-search_settings.getHeight())
-                            .translationY(-search_settings.getHeight()/2)
-                            .scaleY(0f)
-                            .setDuration(ANIMATION_DURATION)
-                            .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
-                            .setListener(new ViewPropertyAnimatorListenerAdapter() {
-                                @Override
-                                public void onAnimationEnd(View view) {
-                                    search_settings.setVisibility(View.GONE);
-                                }
-                            })
-                            .start();
-                } else {
-                    ViewCompat.animate(search_settings)
-                            .alpha(1f)
-                            .translationY(0)
-                            .scaleY(1f)
-                            .setDuration(ANIMATION_DURATION)
-                            .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
-                            .setListener(new ViewPropertyAnimatorListenerAdapter() {
-                                @Override
-                                public void onAnimationStart(View view) {
-                                    search_settings.setVisibility(View.VISIBLE);
-                                }
-                            })
-                            .start();
-                }
-                isOpendSearch = !isOpendSearch;
-            }
-        });
-
-        sort = (Spinner)getActivity().findViewById(R.id.sort);
+        sort = (Spinner) getActivity().findViewById(R.id.sort);
         now_sort = (TextView) getActivity().findViewById(R.id.now_sort);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, spinnerItems);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_custom_layout, spinnerItems);
+        adapter.setDropDownViewResource(R.layout.dialog_custom_layout);
         sort.setAdapter(adapter);
         sort.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -144,5 +117,136 @@ public class SettingsFragment extends Fragment {
             }
         });
         sort.setSelection(SettingsDataStatic.getItemPosition(settingsData.sortType));
+
+        //ジャイロ感度
+        gyro_sensitivity_seekBar = (SeekBar) getActivity().findViewById(R.id.gyro_sensitivity);
+        gyro_sensitivity = (TextView) getActivity().findViewById(R.id.gyro_sensitivity_text);
+
+        //初期値
+        final SettingsData gyro_settingsData = SettingsDataStatic.getInstance();
+        Integer gyro_now = gyro_settingsData.getSearchLimit();
+        gyro_sensitivity.setText(gyro_now.toString());
+        gyro_sensitivity_seekBar.setProgress(gyro_now);
+
+        gyro_sensitivity_seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //ドラッグしたとき
+                Integer result = progress;
+                gyro_sensitivity.setText(result.toString());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //触ったとき
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO: 2017/03/13 DB
+//                Integer result = gyro_sensitivity_seekBar.getProgress();
+//                gyro_settingsData.searchLimit = result;
+//                gyro_settingsData.save();
+
+            }
+        });
+
+        gyro_switch = (Switch) getActivity().findViewById(R.id.gyro_switch);
+        gyro_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO: 2017/03/13 スイッチ
+            }
+        });
+
+        //検索設定
+
+        s_settings_button = (Button) getActivity().findViewById(R.id.s_settings_button);
+        s_settings_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search_settings = (TableLayout) getActivity().findViewById(R.id.search_settings);
+
+                if (isOpendSearch) {
+                    ViewCompat.animate(search_settings)
+                            .alpha(0f)
+                            .translationY(-search_settings.getHeight() / 2)
+                            .scaleY(0f)
+                            .setDuration(ANIMATION_DURATION)
+                            .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
+                            .setListener(new ViewPropertyAnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(View view) {
+                                    search_settings.setVisibility(View.GONE);
+                                }
+                            })
+                            .start();
+                    isOpendSearch = false;
+                } else {
+                    ViewCompat.animate(search_settings)
+                            .alpha(1f)
+                            .translationY(0)
+                            .scaleY(1f)
+                            .setDuration(ANIMATION_DURATION)
+                            .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
+                            .setListener(new ViewPropertyAnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationStart(View view) {
+                                    search_settings.setVisibility(View.VISIBLE);
+                                }
+                            })
+                            .start();
+                    isOpendSearch = true;
+                }
+
+            }
+        });
+
+
+        //センサー設定
+
+        sensor_settings_button = (Button) getActivity().findViewById(R.id.sensor_settings_button);
+        sensor_settings_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sensor_settings = (TableLayout) getActivity().findViewById(R.id.sensor_settings);
+
+                if (isOpendSensor) {
+                    ViewCompat.animate(sensor_settings)
+                            .alpha(0f)
+                            .translationY(-sensor_settings.getHeight() / 2)
+                            .scaleY(0f)
+                            .setDuration(ANIMATION_DURATION)
+                            .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
+                            .setListener(new ViewPropertyAnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(View view) {
+                                    sensor_settings.setVisibility(View.GONE);
+                                }
+                            })
+                            .start();
+                    isOpendSensor = false;
+                } else {
+                    ViewCompat.animate(sensor_settings)
+                            .alpha(1f)
+                            .translationY(0)
+                            .scaleY(1f)
+                            .setDuration(ANIMATION_DURATION)
+                            .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
+                            .setListener(new ViewPropertyAnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationStart(View view) {
+                                    sensor_settings.setVisibility(View.VISIBLE);
+                                }
+                            })
+                            .start();
+                    isOpendSensor = true;
+                }
+
+            }
+        });
+
+
     }
 }
